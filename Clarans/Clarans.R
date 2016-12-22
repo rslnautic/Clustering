228de6da27cb3cbfda4da450697cf93c29071abd
Ramón Serrano López <rslnautic@gmail.com>
2016-12-22 12:36:37 +0100

    changes -_-

--- Clarans/Clarans.R
+++ Clarans/Clarans.R
@@ -1,3 +1,16 @@
 clarans <-function(x, k, metric = "euclidean", stand = FALSE, l = 5, m =10) {
-  
+  changesInMedian <- FALSE; 
+  medians <- data.frame(matrix(, nrow=k, ncol=0))
+  clusters <- 
+  while(l > 0) {
+    repeat {
+      
+      
+      if(!changesInMedian || m <= 0) {
+        break
+      }
+      m <- m - 1
+    }
+    l <- l - 1
+  }
 }
