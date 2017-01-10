8d27c6346d20b74e17c4477df753db11040050fb
Ramón Serrano López <rslnautic@gmail.com>
2017-01-10 01:03:17 +0100

    uyfgolug

--- Clarans/Clarans.R
+++ Clarans/Clarans.R
@@ -7,33 +7,34 @@ clarans <-function(x, k, metric = "euclidean", stand = FALSE, l = 5, m = 10) {
   changesInMedian <- FALSE; 
   xWithMedians <- x;
   xWithMedians["Medians"] <- NA;
+  xWithMedians["DistanceToMedian"] <- NA;
   
   # 1. Repetir l veces
   while(l > 0) {
   # 1. Selecciona k instancias al azar como medianas.
   medians <- randomElements(x, k);
+  
+  iterations = 0
   # 2. Repetir
   repeat {
-    # 1. Re/asignar instancias a la partición con la mediana más próxima
+    
+    # 1. Re/asignar instancias a la partición con la mediana más próxima // TODO: guardar las distancias
     for(i in 1:nrow(x)) {
       distances <- c()
-      value = nrow(merge(x[i,], medians))>0
-      if(!value) {
-        for(j in 1:nrow(medians)) {
-          distances[j] <- euc.dist(x[i,], medians[j,])
-        }
-        xt[i,3] = getPosMinValue(distances)
+
+      for(j in 1:nrow(medians)) {
+        distances[j] <- euc.dist(x[i,], medians[j,])
       }
+      xWithMedians[i,3] = getPosMinValue(distances)
+      xWithMedians[i,4] = min(distances)
+
     }
     # 2. Selecciona una de las medianas al azar y otra instancia del cluster de la mediana al azar.
     randomMedian = randomElements(medians, 1)
-    clusterMedianInstance
-    repeat {
-      clusterMedianInstance = randomElements(xWithMedians, 1)
-      if(!is.na(clusterMedianInstance) && (getPosInDataFrame(randfomMedian, medians) == clusterMedianInstance[,3])) {
-        break
-      }
-    }
+    clusterMedianInstance = NA
+
+    clusterMedianInstance = xWithMedians[sample(which(xWithMedians$Medians==row.name(randomMedian)), 1),]
+      
     # 3. Si la nueva instancia mejora el criterio de error absoluto, se reemplaza la mediana.
     absolutError = 0
     for(i in 1:nrow(medians)) {
@@ -45,15 +46,15 @@ clarans <-function(x, k, metric = "euclidean", stand = FALSE, l = 5, m = 10) {
       }
     }
     
-    if(!changesInMedian || m <= 0) {
+    # Mientras haya cambios en las medianas o se alcance m iteraciones sin cambios
+    if(!changesInMedian || iterations >= m) {
       break
     }
-    m <- m - 1
+    iterations = iterations + 1
   }
   l <- l - 1
   }
     
-  medians = randomElements(x, k);
   return (medians);
 }
 
---
+++ Clarans/ClaransFinal.R
@@ -0,0 +1,43 @@
+
+
+clarans <-function(x, k, metric = "euclidean", stand = FALSE, l = 5, m = 10) {
+  xWithMedians <- x;
+  xWithMedians["Medians"] <- NA;
+  xWithMedians["DistanceToMedian"] <- NA;
+  xWithMedians["TempMedians"] <- NA;
+  xWithMedians["TempDistanceToMedian"] <- NA;
+  
+  while(l > 0) {
+    # 1. Selecciona k instancias al azar como medianas.
+    medians <- randomElements(x, k);
+    
+    iterations = 0
+    # 2. Repetir
+    repeat {
+      
+      # 1. Re/asignar instancias a la partición con la mediana más próxima // TODO: guardar las distancias
+      lapply(x[1:2], 1, function(x) {
+        lapply(medians[1:2], 1, function(y){
+          euc.dist(x,y)
+        })
+      })
+      
+      lapply(x[1:2], 1, function(x) {
+        euc.dist(x, medians[1,]) 
+      })
+      
+      # 2. Selecciona una de las medianas al azar y otra instancia del cluster de la mediana al azar.
+      
+      # 3. Si la nueva instancia mejora el criterio de error absoluto, se reemplaza la mediana.
+      
+      
+      # Mientras haya cambios en las medianas o se alcance m iteraciones sin cambios
+      if(!changesInMedian || iterations >= m) {
+        break
+      }
+      iterations = iterations + 1
+    }
+    l <- l - 1
+  }
+  return (medians);
+}
